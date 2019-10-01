c9114ecf99f3fe5ca5d3cbc643a2e6897c5484f8
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 10:25:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,10 +16,10 @@ class Artist
     song.artist = self
   end
 
-  #def add_song_by_name(name)
-  #  song = Song.new(name)
-  #  song.artist = self
-  #end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
 
   def songs
     Song.all.select {|song| song.artist == self}
