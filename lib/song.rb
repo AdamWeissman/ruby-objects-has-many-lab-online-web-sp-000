b5230f19b2700af1ebdb6466e9e72c61930283ef
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 10:22:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,18 @@
 class Song
-  attr_accessor :title, :artist
-  
-  def initialize(title)
-    @title = title
-    @artist = song.artist(name)
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
 end
