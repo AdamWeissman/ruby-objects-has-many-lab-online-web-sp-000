6fc9ac88ba892fd0965b6a5b4783d1cd948356a6
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 10:23:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,24 +8,24 @@ class Artist
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
+  #def self.all
+  #  @@all
+  #end
 
-  def add_song(song)
-    song.artist = self
-  end
+  #def add_song(song)
+  #  song.artist = self
+  #end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
+  #def add_song_by_name(name)
+  #  song = Song.new(name)
+  #  song.artist = self
+  #end
 
   def songs
     Song.all.select {|song| song.artist == self}
   end
 
-  def self.song_count
-    Song.all.count
-  end
+  #def self.song_count
+  #  Song.all.count
+  #end
 end
